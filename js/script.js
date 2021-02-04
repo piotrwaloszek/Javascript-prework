a9f03f04a091209c01cf9463b3c27371e7bf5206
Piotr Wałoszek <piotrek.waloszek@gmail.com>
2021-02-04 18:22:15 +0100

    Add computer move

--- js/script.js
+++ js/script.js
@@ -7,7 +7,18 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-let computerMove = `kamień`;
-let playerMove = 'papier';
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove +', to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumber == 2){
+	computerMove = "papier";
+} else {
+	computerMove = "nożyczki";
+}
+printMessage ('Mój ruch to:' + computerMove);
