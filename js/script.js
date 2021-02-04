1c5465b9e52b8fd3f7bad13e6126616185df5458
Piotr Wałoszek <piotrek.waloszek@gmail.com>
2021-02-04 18:39:59 +0100

    Add game result

--- js/script.js
+++ js/script.js
@@ -33,8 +33,36 @@ if(playerInput == '1'){
   playerMove = 'kamień';
 } else if (playerInput == '2'){
 	playerMove = 'papier';
-} else {
+} else if (playerInput == '3'){
 	playerMove = 'nożyce';
+} else {
+	printMessage == playerMove;
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == "kamień"){
+	printMessage ('Remis');
+} else if (computerMove == 'kamień' && playerMove =='papier'){
+	printMessage ('Ty wygrywasz');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce'){
+	printMessage ('przegrywasz');
+} else if (computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+	printMessage ('Grasz niezgodnie z zasadami');
+} else if (computerMove == 'papier' && playerMove =='kamień'){
+	printMessage ('przegrywasz');
+} else if (computerMove == 'papier' && playerMove == 'nożyce'){
+	printMessage ('Ty wygrywasz');
+} else if (computerMove == 'papier' && playerMove == 'nieznany ruch'){
+	printMessage ('Grasz niezgodnie z zasadami');
+} else if (computerMove == 'papier' && playerMove == 'papier'){
+	printMessage ('remis');
+} else if (computerMove == 'nożyczki' && playerMove =='kamień'){
+	printMessage ('Ty wygrywasz');
+} else if (computerMove == 'nożyczki' && playerMove == 'papier'){
+	printMessage ('przegrywasz');
+} else if (computerMove == 'nożyczki' && playerMove == 'nieznany ruch'){
+	printMessage ('Grasz niezgodnie z zasadami');
+} else if (computerMove == 'nożyczki' && playerMove == 'nożyce'){
+	printMessage ('remis');
+}
